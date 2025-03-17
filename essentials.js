8add09c8f22baeb4672103acfe90479ad0e62a9e
sheva <olegbeznogy@gmail.com>
2025-03-18 01:22:12 +0300

    Revert "21323"
    This reverts commit a96443bd1a156423887dced284c16877a7a4543b.

--- essentials.js
+++ essentials.js
@@ -5,7 +5,7 @@
 if (location.origin !== "null" && location.protocol !== "file:") {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("webapp/sw.js")
+      .register("sw.js")
       .then(() => console.log("sw registered"))
       .catch((error) => console.error("sw error:", error));
   }
