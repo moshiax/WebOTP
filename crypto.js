1cb7fb6844068bc067cab9e9ed11fa184aa861f0
moshiax <97439761+moshiax@users.noreply.github.com>
2025-05-12 11:19:16 +0300

    keys derivation (not reversecompatible)

--- crypto.js
+++ crypto.js
@@ -129,3 +129,30 @@ function base32ToUint8Array(base32) {
 
   return new Uint8Array(output);
 }
+
+async function deriveKey(password) {
+  const encoder = new TextEncoder();
+
+  const keyMaterial = await crypto.subtle.importKey(
+    "raw",
+    encoder.encode(password),
+    { name: "PBKDF2" },
+    false,
+    ["deriveKey"]
+  );
+
+  const key = await crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt: new Uint8Array(0),
+      iterations: 100000,
+      hash: "SHA-256"
+    },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    false,
+    ["encrypt", "decrypt"]
+  );
+
+  return key;
+}
