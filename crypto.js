a07073461e3f964257a359949aa79c9fe6aec8c9
moshiax <97439761+moshiax@users.noreply.github.com>
2025-10-24 22:28:04 +0300

    icons

--- crypto.js
+++ crypto.js
@@ -66,11 +66,11 @@ function arrayBufferToBase64(buffer) {
   for (let i = 0; i < bytes.byteLength; i++) {
     binary += String.fromCharCode(bytes[i]);
   }
-  return window.btoa(binary);
+  return globalThis.btoa(binary);
 }
 
 function base64ToArrayBuffer(base64) {
-  const binary = window.atob(base64);
+  const binary = globalThis.atob(base64);
   const bytes = new Uint8Array(binary.length);
   for (let i = 0; i < binary.length; i++) {
     bytes[i] = binary.charCodeAt(i);
